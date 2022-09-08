c99b2c7a0679299d57e67133a75d730508e4e136
bigboytimi <olutimiolowookere@icloud.com>
2022-09-08 22:06:55 +0000

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * description : 'writing a C program'
+ *
+ * return : 0
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
