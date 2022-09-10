c8ac8d0ad95f750c14aaa3478a0f5384841593f0
bigboytimi <olutimiolowookere@icloud.com>
2022-09-10 23:13:08 +0000

    Last Digit

---
+++ 0x01-variables_if_else_while/3-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'prints alphabet in lowercase
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while(c <= 'z')
+	{
+		putchar(ch);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
