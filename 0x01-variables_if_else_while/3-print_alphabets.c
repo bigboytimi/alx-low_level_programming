dbb922c430eb4c1e65bca4a9c9e013d608639e7f
bigboytimi <olutimiolowookere@icloud.com>
2022-09-11 13:55:15 +0000

    Print lower and uppercase

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,14 +10,13 @@
 int main(void)
 {
 	int ch;
-
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
-	  putchar (ch);
+	       	putchar (ch);
 	}
 	for (ch = 'A'; ch <= 'Z'; ch++)
 	{
-		putchar (ch);
+		putchar(ch);
 	}
 
 	putchar('\n');
