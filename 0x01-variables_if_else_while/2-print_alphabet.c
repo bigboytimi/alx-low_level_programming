adea494bbe8a73f5625092962e2c9b87ff01f284
bigboytimi <olutimiolowookere@icloud.com>
2022-09-11 12:23:02 +0000

    Print Lowercase Alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'Print lowercase alphabet'
+ *
+ * Return: 0 
+ */
+
+int main(void)
+{
+	int ch;
+
+	for(ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return(0);
+}
+          
