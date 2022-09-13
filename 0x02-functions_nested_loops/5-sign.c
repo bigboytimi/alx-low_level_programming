e8ca8ba5040379b112a90a9dae84164ade34a898
bigboytimi <olutimiolowookere@icloud.com>
2022-09-13 23:20:09 +0000

    signs

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,20 +1,29 @@
 #include "main.h"
+/**
+ * print_sign - Entry point
+ *
+ * Description: 'print signs'
+ *
+ * @n: the recieved integer value
+ *
+ * Return: 1 is true, 0 is false
+ */
 
 int print_sign(int n)
 {
 	if (n > 0)
 	{
 		return (1);
-		_putchar('+');
+		_putchar("+");
 	}
 	else if (n < 0)
 	{
 		return (-1);
-		_putchar('-');
+		_putchar("-");
 	}
 	else
 	{
 		return (0);
-		_putchar('0');
+		_putchar("0");
 	}
 }
