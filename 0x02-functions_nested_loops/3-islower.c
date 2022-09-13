0187817e3c7796afdbc4bcaaac1f8faf1bf93db2
bigboytimi <olutimiolowookere@icloud.com>
2022-09-13 21:58:10 +0000

    Check for lowercase characters

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _islower - Entry point
+ *
+ * Description: 'Lower or higher'
+ *
+ * Return: 0 or 1
+ */
+
+int islower(int c)
+{
+	int i;
+	i = 'a';
+
+	while (i <= 'z')
+	{
+		if (c = i)
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
+		i++;
+	}
+}
