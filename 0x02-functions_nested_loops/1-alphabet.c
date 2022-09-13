fb37f6837de3c262e17c45567b4a709c733408e4
bigboytimi <olutimiolowookere@icloud.com>
2022-09-13 20:33:37 +0000

    print lowercase letters

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -11,10 +11,9 @@ void print_alphabet(void)
 {
 	char alphabet;
 
-	for (alphabet = "a"; alphabet <= "z"; alphabet++)
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 	{
 		_putchar(alphabet);
 	}
 	_putchar('\n');
-	return (0);
 }
