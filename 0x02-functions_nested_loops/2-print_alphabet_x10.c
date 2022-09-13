a41977126939a507a47d75f3a6024770eeb67fd1
bigboytimi <olutimiolowookere@icloud.com>
2022-09-13 21:23:19 +0000

    Print alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * Description: 'print lowercase alphabet 10x'
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	char times, alphabet;
+
+	for (times = 1; times <=10; times++)
+	{
+		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		{
+			_putchar(alphabet);
+		}
+		_putchar('\n');
+		return (void);
+	}
+}
