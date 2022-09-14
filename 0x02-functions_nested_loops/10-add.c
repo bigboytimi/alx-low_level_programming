6a9d86eca92ad02b39c468922429f91628f73906
bigboytimi <olutimiolowookere@icloud.com>
2022-09-14 05:47:36 +0000

    The Adder

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * add - Entry point
+ *
+ * Description: 'add two integers'
+ *
+ * @a: placeholder for value
+ *
+ * @b: placeholder for value
+ *
+ * Return: Always 0
+ */
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
