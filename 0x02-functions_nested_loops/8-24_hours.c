e33f8682ed29caf2134bbccd890bd5dcbde9fe1e
bigboytimi <olutimiolowookere@icloud.com>
2022-09-14 01:07:18 +0000

    Jack Buaer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,6 +10,9 @@ void jack_bauer(void)
 {
 	int hour, minute;
 
+	for (hour =0; hour <= 23; hour++)
+	{
+
 		for (minute = 0; minute <= 59; minute++)
 		{
 			_putchar((hour / 10) + '0');
@@ -19,4 +22,6 @@ void jack_bauer(void)
 			_putchar((minute % 10) + '0');
 			_putchar('\n');
 		}
+
+	}
 }
