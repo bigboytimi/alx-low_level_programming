3ddf2b1da80966114bf5f09834a3bc9bfbc8c951
bigboytimi <olutimiolowookere@icloud.com>
2022-09-14 00:04:58 +0000

    calculate absolute value of an integer

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -4,20 +4,20 @@
  *
  * Description: 'compute the absolute value of an integer'
  *
- * int: data type to be recieved
+ * @n: placeholder for value
  *
- * Return 0
+ * Return: 0
  */
 
 int _abs(int n)
 {
 	if (n > 0)
 	{
-		return(n);
+		return (n);
 	}
 	else if (n < 0)
 	{
-		return(-n);
+		return (-n);
 	}
 	else
        	{
