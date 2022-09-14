09489b84700786d40c5bfce6c4901797563e4a4c
bigboytimi <olutimiolowookere@icloud.com>
2022-09-14 05:37:25 +0000

    9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,8 +14,8 @@ void times_table(void)
 	for (num = 0; num <= 9; num++)
 	{
 		_putchar('0');
-		
-		
+
+
 		for (mult = 1; mult <= 9; mult++)
 		{
 			_putchar(',');
@@ -25,11 +25,11 @@ void times_table(void)
 			prod = num * mult;
 
 
-				if (prod <= 9) 
+				if (prod <= 9)
 					_putchar(' ');
 				else
 					_putchar((prod / 10) + '0');
-			
+
 			_putchar((prod % 10) + '0');
 
 
