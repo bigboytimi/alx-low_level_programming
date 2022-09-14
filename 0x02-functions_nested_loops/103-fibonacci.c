a93be12c54bd644db729ef74f55ac53315cb040b
bigboytimi <olutimiolowookere@icloud.com>
2022-09-14 09:00:19 +0000

    103-fibonacci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/*
+ * main - Entry point
+ *
+ * Description: 'fibonacci sequence for even numbers'
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	float count;
+	unsigned long fib1 = 0, fib2 = 1;
+
+	while(1)
+	{
+		fibsum = fib1 + fib2;
+		if (fibsum > 4000000)
+			break;
+
+		if ((fibsum % 2) == 0)
+			count += fibsum;
+
+		fib1 = fib2;
+		fib2 = fibsum;
+
+	}
+	printf("%.0f\n", count);
+
+	return (0);
+}
