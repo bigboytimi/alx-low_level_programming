5f0d9256c3fb8f1057494b4b76672222e10d81e8
bigboytimi <olutimiolowookere@icloud.com>
2022-09-14 07:52:00 +0000

    sum of multiples of 1024

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'add all natural number that are multiples of 3 and 5'
+ *
+ * @i: placeholder for value
+ *
+ * @sum : value storage
+ *
+ * Return: 0
+ */
+int main (void)
+{
+	int i, sum;
+
+	for (i = 0; i <= 1024; i++)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+		{
+			sum += i;
+		}
+		printf("%d\n", sum);
+		return (0);
+	}
+}
+
