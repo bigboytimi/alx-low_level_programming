16d6cc8a72dec080c343e7f73aaecdb290443346
bigboytimi <olutimiolowookere@icloud.com>
2022-09-15 21:06:27 +0000

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,28 +8,29 @@
  * Return: 0
  */
 
-int main()
+int main(void)
 {
 	int i;
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0)
+		if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("Fizz ");
+			printf("FizzBuzz ");
 		}
 		else if (i % 5 == 0)
 		{
 			printf("Buzz ");
 		}
-		else if (i % 5 == 0 && i % 3 == 0)
+		else if (i % 3 == 0)
 		{
-			printf("FizzBuzz ");
+			printf("Fizz ");
 		}
 		else
 		{
 			printf("%d ", i);
 		}
 	}
+	putchar('\n');
 	return (0);
 }
