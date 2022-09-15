b89bb08da997ff70ad34b5c51c96e7471c3bffec
bigboytimi <olutimiolowookere@icloud.com>
2022-09-15 18:54:30 +0000

    print lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_line - print line
+ *
+ * Return: Always 0.
+ */
+void print_line(int n)
+{
+	char c = '_';
+	int i = 1;
+
+	while (i <= n && n != 0)
+	{
+		_putchar(c);
+		i++;
+	}
+
+	_putchar('\n');
+}
+
