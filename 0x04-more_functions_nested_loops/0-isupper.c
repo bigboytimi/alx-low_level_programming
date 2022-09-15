c8775f4efb33daae6df0bd23020550abe09b9481
bigboytimi <olutimiolowookere@icloud.com>
2022-09-15 11:44:42 +0000

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _isupper - writes a function that checks for uppercase character
- *
+ * @c : placeholder for received value
  * Return: 1 is uppercase. 0 is otherwise
  */
 
