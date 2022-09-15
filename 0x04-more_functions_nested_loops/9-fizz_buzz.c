64ee2d3ca8f0b658ec4287979dda1dc0c13431b2
bigboytimi <olutimiolowookere@icloud.com>
2022-09-15 20:55:14 +0000

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: FizzBuzz Challenge
+ *
+ * Return: 0
+ */
+
+int main()
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (i % 5 == 0 && i % 3 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	return (0);
+}
