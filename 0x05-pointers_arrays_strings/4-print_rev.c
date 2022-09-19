e6a7a3198ab381b5b13da835008a97c7ba64f34c
bigboytimi <olutimiolowookere@icloud.com>
2022-09-19 20:11:40 +0000

    print a string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_rev - print a string in reverse
+ *
+ * @s: pointer to s
+ *
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+
+	int count = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		count++;
+	}
+	
+	while (count > 0)
+	{
+		s--;
+		_putchar(*s);
+		count--;
+	}
+}
