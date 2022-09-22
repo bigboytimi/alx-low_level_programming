96810903d2c5321dcae05d0bca6ddde1799bcb46
bigboytimi <olutimiolowookere@icloud.com>
2022-09-22 20:20:53 +0000

    change lowercase to uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -7,15 +7,17 @@
  *
  * Return: string
  */
+
 char *string_toupper(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] == '\0'; i++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (str[i] >= 'a' && str[i] <= 'z')
 		{
 			str[i] -= 32;
 		}
 	}
+	return (str);
 }
