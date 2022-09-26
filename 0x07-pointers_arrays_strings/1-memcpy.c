cb4260276c8a0d003399ee95e7e28787d17e006a
bigboytimi <olutimiolowookere@icloud.com>
2022-09-26 19:38:49 +0000

    copy memory area

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies memory area
+ * @dest: destination to copy to
+ * @src: source to copy from
+ * @n: number of bytes to copy
+ * Return: dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	i = 0;
+
+	while (n > i)
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	return (dest);
+}
