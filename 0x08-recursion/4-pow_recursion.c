4b3ab8bd3076e504ec9945d471229be8df0e3383
bigboytimi <olutimiolowookere@icloud.com>
2022-09-28 17:11:23 +0000

    returns value x raised to y

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - return the value of x raised to power y
+ *
+ * @x: argument
+ *
+ * @y: argument
+ *
+ * Return: integer
+ */
+
+int _pow_recursion(int x, int y)
+{
+	/* base case */
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (x == 0 || y == 0)
+	{
+		return (1);
+	}
+	return (x * _pow_recursion(x, y-1));
+}
