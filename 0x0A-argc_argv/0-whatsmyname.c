97426afad9297f48838b6fcf0bdbccdb24b6f2ac
bigboytimi <olutimiolowookere@icloud.com>
2022-10-01 01:51:56 +0000

    0 - argc argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * @argc: argument count
+ *
+ * @argv: argument vector
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int count;
+
+	if (argc >= 1)
+	{
+		for (count = 0; count < argc; count++)
+		{
+			printf("%s\n", argv[count]);
+		}
+	}
+	return (0);
+}
