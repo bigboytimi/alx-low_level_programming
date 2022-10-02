ca1e45ae3de612053e669c6bc6d3b1f406a4f19d
bigboytimi <olutimiolowookere@icloud.com>
2022-10-02 19:29:52 +0000

    100-change

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ *
+ * @argc: argument count
+ *
+ * @argv: argument vector
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int num, result, j;
+	int coins[] = {25, 10, 5, 2, 1};
+
+	num = atoi(argv[1]);
+	result = 0;
+
+	if (num < 0)
+	{
+		printf("0\n");
+	}
+	else if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	for (j = 0; j < 5 && num >= 0; j++)
+	{
+		while (num >= coins[j])
+		{
+			result++;
+			num -= coins[j];
+		}
+	}
+	printf("%d\n", result);
+	return (0);
+}
