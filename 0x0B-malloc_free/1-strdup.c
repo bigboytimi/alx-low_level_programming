cb6f3bf8bf555cd7f30959912bfda9fe9fecd165
bigboytimi <olutimiolowookere@icloud.com>
2022-10-03 14:58:46 +0000

    string duplicate

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - returns pointer to a duplicated string
+ *
+ * @str: string
+ *
+ * Return: 0, if str is NULL. pointer, if valid
+ */
+
+char *_strdup(char *str)
+{
+	char *s;
+	unsigned int i = 0;
+	unsigned int j = 0;
+
+	/* check if str contains a value */
+	if (*str == 0)
+	{
+		return (0);
+	}
+	/* get size of the str */
+	while (str[i] != '\0')
+	i++;
+
+	/* allocate memory and assign it to pointer s */
+	s = malloc(sizeof(char) * i - 1);
+
+	if (*s == 0)
+	{
+		return (0);
+	}
+
+	while (j < i)
+	{
+		s[j] = str[j];
+		j++;
+	}
+	s[j] == '\0';
+	return (s);
+}
+
+
+
+
+
