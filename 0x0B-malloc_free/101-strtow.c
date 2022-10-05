8b2348fa9246ddb5956e5ec0015c9ccd78344add
bigboytimi <olutimiolowookere@icloud.com>
2022-10-05 21:09:21 +0000

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -62,7 +62,8 @@ int count_words(char *str)
 char **strtow(char *str)
 {
 	char **strings;
-	int index = 0, words, w, letters, l;
+	int index = 0, words, letters, l;
+	int w;
 
 	if (str == NULL || str[0] == '\0')
 		return (NULL);
