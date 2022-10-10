c1d6b770ce0d7e419198a9998cafe2c4fbc06034
bigboytimi <olutimiolowookere@icloud.com>
2022-10-10 19:30:44 +0000

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,19 @@
+#ifndef dog_h
+#define dog_h
 
-typedef struct dog 
+/**
+ * struct dog - Structure for a dog
+ * @name: 1st member
+ * @age: 2nd member
+ * @owner: 3rd member
+ *
+ * Description: A structure with elements
+ */
+typedef struct dog
 {
-	char* name;
+	char *name;
 	float age;
-	char* owner;
+	char *owner;
 } dog;
+
+#endif
