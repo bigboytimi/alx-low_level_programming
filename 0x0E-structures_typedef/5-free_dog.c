5ac8e44ea8075579e0e7639630ceac94b3602bd2
bigboytimi <olutimiolowookere@icloud.com>
2022-10-11 23:28:53 +0000

    free dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - a function that frees dogs
+ *
+ * @d: dog to be freed
+ *
+ * Return: void
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
