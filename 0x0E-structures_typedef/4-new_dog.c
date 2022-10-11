0a9188a0140dfc5897265c9dcb9df0eb7b944bf1
bigboytimi <olutimiolowookere@icloud.com>
2022-10-11 21:58:39 +0000

    create new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * new_dog - function entry point
+ *
+ * @name: name of the dog
+ *
+ * @age: age of the dog
+ *
+ * @owner: owner of the dog
+ *
+ * Description: write a function that creates a new dog
+ *
+ * Return: pointer, on success. NULL, if functions fail
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *doggy;
+	unsigned int i, j, count;
+
+	if (name == NULL || owner == NULL)
+		return (NULL);
+
+	doggy = malloc(sizeof(dog_t));
+
+	if (doggy == NULL)
+		return (NULL);
+
+	for (i = 0; name[i] != '\0'; i++)
+		i++;
+
+	doggy->name = malloc(sizeof(char) * i);
+
+	if (doggy->name == NULL)
+	{
+		free(doggy);
+		return (NULL);
+	};
+
+	for (count = 0; count < i; count++)
+		doggy->name[count] = name[count];
+
+	doggy->age = age;
+
+	for (j = 0; j != '\0'; j++)
+		j++;
+
+	doggy->owner = malloc(sizeof(char) * j);
+
+	if (doggy->owner == NULL)
+	{
+		free(doggy);
+		free(doggy->name);
+		return (NULL);
+	};
+
+	for (count = 0; count < j; count++)
+		doggy->owner[j] = owner[j];
+
+	return (doggy);
+}
