529849c9eb397a5e2f058c5d34c600a5ea4104a4
bigboytimi <olutimiolowookere@icloud.com>
2022-10-12 14:49:09 +0000

    array iterator

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -17,10 +17,10 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	if (array == NULL || action == NULL)
-		return;
-
 	unsigned int i;
+
+	if (array == NULL || action == NULL || size == 0)
+		return;
 
 	for (i = 0; i < size; i++)
 	{
