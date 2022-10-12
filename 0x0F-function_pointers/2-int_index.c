8c531b188d5272787ae0d2bc683d309ec2b91e1f
bigboytimi <olutimiolowookere@icloud.com>
2022-10-12 15:54:47 +0000

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -22,13 +22,13 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	for (i = 0; i < size; i++)
 	{
-		if (cmp(array[i] == 0))
+		if (cmp(array[i] != 0))
 		{
-			return (-1);
+			return (i);
 		}
 		else
 		{
-			return (i);
+			return (-1);
 		}
 	}
 }
