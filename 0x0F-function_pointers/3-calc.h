3e458c049e7a68164fdc1cea28504b008d375da6
bigboytimi <olutimiolowookere@icloud.com>
2022-10-12 22:41:41 +0000

    perform simple operations

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,30 +1,13 @@
-#ifndef CALC_H
-#define CALC_H
 int op_add(int a, int b);
-#endif
 
-#ifndef CALC_H
-#define CALC_H
 int op_sub(int a, int b);
-#endif
 
-#ifndef CALC_H
-#define CALC_H
 int op_mul(int a, int b);
-#endif
 
-#ifndef CALC_H
-#define CALC_H
 int op_div(int a, int b);
-#endif
 
-#ifndef CALC_H
-#define CALC_H
 int op_mod(int a, int b);
-#endif
 
-#ifndef CALC_H
-#define CALC_H
 /**
  * struct op - Struct op
  *
@@ -37,16 +20,8 @@ typedef struct op
 	int (*f)(int a, int b);
 } op_t;
 
-#ifndef CALC_H
-#define CALC_H
 
 int (*get_op_func(char *s))(int, int);
 
-#endif
-
-#ifndef CALC_H
-#define CALC_H
 
 int main(int argc, char *argv[]);
-
-#endif
