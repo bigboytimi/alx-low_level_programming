5daaa5e4141b924805669b16745468b045f6f654
bigboytimi <olutimiolowookere@icloud.com>
2022-10-20 12:32:10 +0000

    printlist

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * print_list - prints all elements of a list_t list
+ * @h: head node
+ * Return: number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t i;
+
+	i = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)");
+		}
+
+		printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,3 @@
+# DATA STRUCTURES: Singly Linked Lists in C
+
+- This folder contains solution codes for my tasks in ALX for Singly-linked lists
