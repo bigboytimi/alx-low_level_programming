811c539235fe330b6ef56c62ed8118ff204189dc
bigboytimi <olutimiolowookere@icloud.com>
2022-10-20 13:11:55 +0000

    print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,7 +14,7 @@ size_t print_list(const list_t *h)
 	size_t i;
 
 	i = 0;
-       
+
 	while (h != NULL)
 	{
 		if (h->str == NULL)
@@ -25,7 +25,7 @@ size_t print_list(const list_t *h)
 
 		h = h->next;
 		i++;
-		
+
 	}
 	return (i);
 }
