52f5e1a47ce6878dc3bc134aee7ff2b525873fbc
bigboytimi <olutimiolowookere@icloud.com>
2022-10-20 23:08:47 +0000

    free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * free_list - frees a list_t list
+ *
+ * @head: head node
+ *
+ * Return: void
+ */
+
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
+}
