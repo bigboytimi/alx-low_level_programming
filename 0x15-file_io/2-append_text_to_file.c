22ef5e1ea906e81a14e62a4d78f96dd619eede73
bigboytimi <olutimiolowookere@icloud.com>
2022-10-31 14:57:27 +0000

    append text

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -16,7 +16,7 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int w, fd, s = 0;
+	int fd, s = 0;
 
 	if (filename == NULL)
 		return (-1);
@@ -32,7 +32,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	{
 		while (text_content[s])
 			s++;
-		w = write(fd, text_content, s);
+		write(fd, text_content, s);
 		return (1);
 	}
 
