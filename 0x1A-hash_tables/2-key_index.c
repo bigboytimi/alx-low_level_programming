7b59356a606303e72540f26ddb8038f32e8d21cc
bigboytimi <olutimiolowookere@icloud.com>
2023-01-06 07:44:05 +0000

    key_index

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -12,13 +12,5 @@
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	unsigned long int hash;
-	int c;
-
-	hash = 5381;
-	while ((c = *key++))
-	{
-		hash = ((hash << 5) + hash) + c /* hash * 33 + c */
-	}
-	return (hash % size);
+	return (hash_djb2(key) % size);
 }
