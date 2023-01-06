c6c95e9671d47d5e3062d1abc072ce0a46c43c29
bigboytimi <olutimiolowookere@icloud.com>
2023-01-06 17:48:30 +0000

    5-print

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,39 @@
+#include "./hash_tables.h"
+
+/**
+ * hash_table_print -prints a hash table
+ * @ht: A pointer to the hash table to print.
+ *
+ * Return: key/value pairs
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *node;
+	unsigned long int i;
+	unsigned char comma_check = 0;
+
+	if (ht == NULL)
+		return;
+
+	printf("{")
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i] != NULL)
+		{
+			if (comma_check == 1)
+				printf(", ");
+
+			node = ht->array[i];
+			while (node != NULL)
+			{
+				printf("'%s': '%s'", node->key, node->value);
+				node = node->next;
+				if (node != NULL)
+					printf(", ");
+			}
+			comma_check = 1;
+		}
+	}
+	printf("}\n");
+}
